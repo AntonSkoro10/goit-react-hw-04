725799559873811a6e4bb5eafe3ee762e34d8092
AntonSkoro10 <antonskoro112@gmail.com>
2024-08-16 15:39:41 +0300

    update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,9 +2,10 @@ import css from "./ImageGallery.module.css";
 import ImageCard from "../ImageCard/ImageCard";
 
 export default function ImageGallery({ imageArr, openModal }) {
+  console.log(imageArr); 
   return (
     <ul className={css.list}>
-      {imageArr.results.map((image) => (
+      {imageArr.map((image) => ( 
         <li key={image.id}>
           <ImageCard image={image} openModal={openModal} />
         </li>
--- src/unsplash-api.js
+++ src/unsplash-api.js
@@ -1,22 +1,17 @@
+// unsplash-api.js
 import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
 export default async function fetchImages(query, currentPage) {
-  try {
-    const result = await axios.get('/search/photos', {
-      params: {
-        query: query,
-        page: currentPage,
-        per_page: 10,
-        client_id: "5WuXKwal-H-QLgFnOzc1HciolmvfhFjDUHMUQgmFyYw",
-        orientation: "landscape", 
-      }
-    });
-    console.log("API result:", result.data); 
-    return result.data;
-  } catch (error) {
-    console.error("API error:", error);
-    return { results: [] }; 
-  }
+  const result = await axios.get('/search/photos', {
+    params: {
+      query: query,
+      page: currentPage,
+      per_page: 10,
+      client_id: "5WuXKwal-H-QLgFnOzc1HciolmvfhFjDUHMUQgmFyYw",
+      orientation: "landscape",
+    }
+  });
+  return result.data;
 }
